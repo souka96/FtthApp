9b4f252a2e7353954ca75d99da89ce12a60d72ea
souka96 <soukaina.zouyne34@gmail.com>
2018-07-04 13:12:31 +0000

    Search view has been deleted nd new activities added

--- app/src/main/java/com/ezzouyn/soukaina/ftthapp/AllProjectsActivity.java
+++ app/src/main/java/com/ezzouyn/soukaina/ftthapp/AllProjectsActivity.java
@@ -3,11 +3,11 @@ package com.ezzouyn.soukaina.ftthapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+public class AllProjectsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_all_projects);
     }
 }
--- app/src/main/java/com/ezzouyn/soukaina/ftthapp/MainActivity.java
+++ app/src/main/java/com/ezzouyn/soukaina/ftthapp/MainActivity.java
@@ -1,26 +1,20 @@
 package com.ezzouyn.soukaina.ftthapp;
 
 import android.app.Dialog;
-import android.app.SearchManager;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.SearchView;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap mGoogleMap ;
-    MaterialSearchView searchView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,72 +22,38 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
              setContentView(R.layout.activity_main);
              initMap();
         }
-        else{}
-        searchView = (MaterialSearchView) findViewById(R.id.search_view);
-        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //Do some magic
-                return false;
-            }
+        else{
+        }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //Do some magic
-                return false;
-            }
-        });
-
-        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
-            @Override
-            public void onSearchViewShown() {
-                //Do some magic
-            }
-
-            @Override
-            public void onSearchViewClosed() {
-                //Do some magic
-            }
-        });
     }
-
     private void initMap(){
         MapFragment mapFragment =(MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
-
     }
-
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menuu, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        searchView.setMenuItem(item);
-        // Retrieve the SearchView and plug it into SearchManager
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
-        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        return true;
-    }
-
-    public boolean googleServicesAvailable(){
+    public boolean googleServicesAvailable() {
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvailable = api.isGooglePlayServicesAvailable(this);
-        if(isAvailable== ConnectionResult.SUCCESS){
+        if (isAvailable == ConnectionResult.SUCCESS) {
             return true;
-        }
-        else if(api.isUserResolvableError(isAvailable)){
-            Dialog dialog = api.getErrorDialog(this,isAvailable,0);
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
             dialog.show();
-        }
-        else{
-            Toast.makeText(this,"can not connect to google services",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "can not connect to google services", Toast.LENGTH_LONG).show();
         }
         return false;
     }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap=googleMap;
-
     }
-}
+    private void goToLocation(double lat, double lng) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mGoogleMap.moveCamera(update);
+    }
+    private void goToLocationZoom(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        mGoogleMap.moveCamera(update);
+    }}
